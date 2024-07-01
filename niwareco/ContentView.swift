96f31d93a9c650cd110116e138d582bd0960bd04
carpbream <idamaiko+@gmail.com>
2024-07-02 08:47:59 +0900

    test commit

--- niwareco/ContentView.swift
+++ niwareco/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, niwareco!")
+            Text("Good bye, niwareco!")
         }
         .padding()
     }
